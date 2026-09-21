15db07c4fb7a6ad6b49cdd4fda66cfb3a1a0b6e0
Jordan Evans <jordan.evans@correla.com>
2026-09-21 19:29:40 +0100

    feat: remove boilerplate from API

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
+from fastapi import status
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/application", status_code=status.HTTP_501_NOT_IMPLEMENTED)
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+    # TODO: Implement slim view that dispatches application submission to command bus
+    return status.HTTP_501_NOT_IMPLEMENTED
